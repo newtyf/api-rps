37e27addea367b0a4c3035da34eb89f881aad2bf
newt <axelskam.123@outlook.es>
2023-03-23 21:22:32 -0500

    init pass to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+import "dotenv/config"
+import express from "express"
+import cors from "cors";
+
 const { dbConnectRedis } = require("./config/redis");
 const app = express();
 const PORT = process.env.PORT || 3002;
