df3d2af16cf8b07ea8decf2233790dd4c0467ed5
newt <axelskam.123@outlook.es>
2024-03-17 22:53:35 -0500

    add dynamic cors

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,22 @@ import { router } from "./routes";
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-app.use(cors());
+const whitelist = ['https://rps.newtyf.com/']
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (origin === undefined) {
+      return callback(new Error('origin no defnido'))
+    }
+
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200
+}));
 app.use(express.json());
 // app.enable("trust proxy")
 
